8133e4ae476177f28c923144473059b3891628c6
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-21 14:21:33 +0900

    feat: Add base time entity

---
+++ src/main/java/com/example/demo/common/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.example.demo.common.domain;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class BaseTimeEntity {
+
+	private LocalDateTime localDateTime;
+
+	public BaseTimeEntity(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+
+	public String getDateTimeToString() {
+		return localDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+	}
+
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+}
