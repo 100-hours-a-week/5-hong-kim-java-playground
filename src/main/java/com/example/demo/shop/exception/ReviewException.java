f5ad737d45682619473dc3b2c3bb6f14430821b5
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-23 23:38:17 +0900

    feat: Add review global exception

---
+++ src/main/java/com/example/demo/shop/exception/ReviewException.java
@@ -0,0 +1,11 @@
+package com.example.demo.shop.exception;
+
+import com.example.demo.common.exception.BaseException;
+import com.example.demo.common.exception.BaseExceptionType;
+
+public class ReviewException extends BaseException {
+
+	public ReviewException(BaseExceptionType baseExceptionType) {
+		super(baseExceptionType);
+	}
+}
---
+++ src/main/java/com/example/demo/shop/exception/ReviewExceptionType.java
@@ -0,0 +1,18 @@
+package com.example.demo.shop.exception;
+
+import com.example.demo.common.exception.BaseExceptionType;
+
+public enum ReviewExceptionType implements BaseExceptionType {
+	NOT_EXIST_REVIEW("존재하는 리뷰가 없습니다");
+
+	private final String message;
+
+	ReviewExceptionType(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String message() {
+		return message;
+	}
+}
