1eba719fd6ee154e8acd97c08dbee6a07ed3d266
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-25 23:25:44 +0900

    feat: Add event consumer dispatch to find appropriate consumer

---
+++ src/main/java/com/example/demo/async/EventConsumerDispatch.java
@@ -0,0 +1,31 @@
+package com.example.demo.async;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.example.demo.common.domain.BaseTimeEntity;
+import com.example.demo.member.MemberContainer;
+import com.example.demo.member.domain.Member;
+import com.example.demo.shop.ShopContainer;
+import com.example.demo.shop.domain.Review;
+import com.example.demo.shop.domain.keyboard.Keyboard;
+import com.example.demo.shop.domain.laptop.Laptop;
+import com.example.demo.shop.domain.monitor.Monitor;
+
+public class EventConsumerDispatch {
+
+	private static final Map<Class<? extends BaseTimeEntity>, EventConsumer<? extends BaseTimeEntity>> handlers =
+		new ConcurrentHashMap<>();
+
+	static {
+		handlers.put(Member.class, MemberContainer.memberRepository());
+		handlers.put(Monitor.class, ShopContainer.orderRepository());
+		handlers.put(Laptop.class, ShopContainer.orderRepository());
+		handlers.put(Keyboard.class, ShopContainer.orderRepository());
+		handlers.put(Review.class, ShopContainer.reviewRepository());
+	}
+
+	public static <T> EventConsumer findFitConsumer(Class<T> clazz) {
+		return handlers.get(clazz);
+	}
+}
